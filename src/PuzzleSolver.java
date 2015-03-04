229c2d28ef258186c6aa71b1e9d3d3aa9997bbd5
baldaz <a.g.baldan@gmail.com>
2015-03-04 12:10:21 +0100

    IO

--- src/PuzzleSolver.java
+++ src/PuzzleSolver.java
@@ -1,19 +1,9 @@
 import puzzlesolver.*;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class PuzzleSolver {
 
     public static void main(String args[]) {
         Puzzle puzzle = new Puzzle();
-        // puzzle = readFile(Paths.get(args[0]));
         IPuzzleView view = new PuzzleView(new IOPuzzle(args[0]));
         puzzle = view.inputPuzzle();
         IPuzzleController controller = new PuzzleController(puzzle, view);
